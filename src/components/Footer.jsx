8a7999c5c4991ca2ace84765ad33fc93f669a90d
nawaid2003 <md.nawaidsheikh@gmail.com>
2025-05-01 18:17:36 +0530

    footerlogo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
 import styles from "../styles/footer.module.scss";
+import Image from "next/image";
+import logoSL from "../images/logoSL.png";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.footerSection}>
-          <h3>Silver Lining</h3>
-          <p>Where Hope Meets Elegance.</p>
+          <div className={styles.logoContainer}>
+            <div className={styles.logoImage}>
+              <Image
+                src={logoSL}
+                alt="Silver Lining Logo"
+                width={60}
+                height={60}
+              />
+            </div>
+            <div className={styles.logoText}>
+              <h3>Silver Lining</h3>
+              <p>Where Hope Meets Elegance.</p>
+            </div>
+          </div>
         </div>
         <div className={styles.footerSection}>
           <h3>Explore</h3>
